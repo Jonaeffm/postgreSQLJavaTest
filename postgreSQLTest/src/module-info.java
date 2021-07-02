40287c095a019b1348ed22a7f87a168f13d28f46
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2021-07-02 22:36:16 +0200

    Add files via upload
    add method to connect to postgreSQL

--- postgreSQLTest/src/module-info.java
+++ postgreSQLTest/src/module-info.java
@@ -1,2 +1,3 @@
 module postgreSQLTest {
+	requires java.sql;
 }
--- postgreSQLTest/src/postgreSQLTest/main.java
+++ postgreSQLTest/src/postgreSQLTest/main.java
@@ -1,10 +1,26 @@
 package postgreSQLTest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world");
-	}
 
+		   public static void main(String args[]) {
+		      Connection c = null;
+		      try {
+		         Class.forName("org.postgresql.Driver");
+		         c = DriverManager
+		            .getConnection("jdbc:postgresql://localhost:5432/testdb",
+		            "postgres", null);
+		      } catch (Exception e) {
+		         e.printStackTrace();
+		         System.err.println(e.getClass().getName()+": "+e.getMessage());
+		         System.exit(0);
+		      }
+		      System.out.println("Opened database successfully");
+		   }
+		
 }
