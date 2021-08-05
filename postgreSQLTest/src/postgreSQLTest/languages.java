8aae7bd9cdbef0d683b62dd979ef856fde9aef7a
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2021-08-05 22:45:05 +0200

    Add files via upload
    starting to use Hibernate

---
+++ postgreSQLTest/src/postgreSQLTest/languages.java
@@ -0,0 +1,33 @@
+package postgreSQLTest;
+
+public class languages {
+	private int id;
+	private String e;
+	private String g;
+	private String n;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getE() {
+		return e;
+	}
+	public void setE(String e) {
+		this.e = e;
+	}
+	public String getG() {
+		return g;
+	}
+	public void setG(String g) {
+		this.g = g;
+	}
+	public String getN() {
+		return n;
+	}
+	public void setN(String n) {
+		this.n = n;
+	}
+	
+}
