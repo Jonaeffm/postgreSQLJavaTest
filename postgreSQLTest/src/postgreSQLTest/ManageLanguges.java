1584e2d02ec5dbd6739b00383a21bb46a07f33ec
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2021-08-06 02:04:12 +0200

    Add files via upload

--- postgreSQLTest/src/postgreSQLTest/ManageLanguges.java
+++ postgreSQLTest/src/postgreSQLTest/ManageLanguges.java
@@ -1,5 +1,27 @@
 package postgreSQLTest;
 
-public class ManageLanguges {
+import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Iterator;
+
+
+
+
+public class ManageLanguges {
+	private static SessionFactory factory;
+	public void manage()
+	{
+		try {
+			factory = new Configuration().configure().buildSessionFactory();
+		}catch (Throwable ex)
+		{
+			System.err.println("Failed to create sessionFactory object"+ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+		
+		
+	}
 }
